a0137d69d9c123da2ed722068e8133a53cdf0313
hemant11ks <ksingh1617@gmail.com>
2025-01-31 19:49:16 +0530

    finalllh

--- app/main.py
+++ app/main.py
@@ -1,25 +1,108 @@
-from app.lexer import Lexer
-import argparse
+from argparse import ArgumentParser, Namespace
+import sys
+
+from .tokens import Tokenizer
+from .parse import Parser
+from .execution import ExecutionScope
+from .utils import RuntimeError
+
+def main():
+    args = parse_args()
+    args.entry(args)
+    # print(args)
+
+def parse_args() -> Namespace:
+    arg_parser = ArgumentParser()
+    sub_parser = arg_parser.add_subparsers()
+    
+    arg_parser.add_argument("file")
+    config_tokenize_parser(sub_parser.add_parser("tokenize"))
+    config_parse_parser(sub_parser.add_parser("parse"))
+    config_evaluate_parser(sub_parser.add_parser("evaluate"))
+    config_execute_parser(sub_parser.add_parser("run"))
+    
+    
+    return arg_parser.parse_args()
 
 
-def main() -> None:
-    arg_parser = argparse.ArgumentParser()
-
-    command_arg_parser = arg_parser.add_subparsers(dest='command', required=True)
-
-    tokenize_arg_parser = command_arg_parser.add_parser('tokenize')
-    tokenize_arg_parser.add_argument('filename')
-
-    args = arg_parser.parse_args()
-
-    if args.command == 'tokenize':
-        with open(args.filename, 'rb') as f:
-            lexer = Lexer(f, True)
-            lexer.tokenize()
-
-        if lexer.has_errors:
-            exit(65)
 
 
-if __name__ == '__main__':
+def config_parse_parser(arg_parser: ArgumentParser) -> None:
+    arg_parser.set_defaults(entry=print_parse_result)
+    
+def config_evaluate_parser(arg_parser: ArgumentParser) -> None:
+    arg_parser.set_defaults(entry=print_evalute_result)
+    
+def config_tokenize_parser(arg_parser: ArgumentParser) -> None:
+    arg_parser.set_defaults(entry=print_tokens)
+
+def config_execute_parser(arg_parser: ArgumentParser) -> None:
+    arg_parser.set_defaults(entry=execute_file)
+    
+    
+def print_parse_result(ns: Namespace) -> None:
+    with open(ns.file) as fd:
+        file_contents = fd.read()
+    
+    parser = Parser(file_contents)
+    for scope, expression in parser:
+        print(expression)
+    
+    if parser.error:
+        exit(65)
+
+
+def print_evalute_result(ns: Namespace) -> None:
+    with open(ns.file) as fd:
+        file_contents = fd.read()
+    
+    parser = Parser(file_contents)
+    try:
+        for scope, expression in parser:
+            value = expression.evaluate(scope)
+            if isinstance(value, bool):
+                print(str(value).lower())
+            elif value is None:
+                print("nil")
+            else:
+                print(value)
+    except RuntimeError as e:
+        print(e, file=sys.stderr)
+        exit(70)
+    
+    if parser.error:
+        exit(65)
+
+
+def print_tokens(ns: Namespace) -> None:
+    with open(ns.file) as fd:
+        file_contents = fd.read()
+    
+    tokenized = Tokenizer(file_contents)
+    for token in tokenized:
+        print(token)
+    
+    if tokenized.error:
+        exit(65)
+    
+    
+def execute_file(ns: Namespace) -> None:
+    with open(ns.file) as fd:
+        file_contents = fd.read()
+    
+    parser = Parser(file_contents)
+    parse_results = list(parser)
+    if parser.error:
+        exit(65)
+    
+    try:
+        for scope, expression in parse_results:
+            expression.evaluate(scope)
+    except RuntimeError as e:
+        print(e, file=sys.stderr)
+        exit(70)
+
+
+
+if __name__ == "__main__":
     main()
